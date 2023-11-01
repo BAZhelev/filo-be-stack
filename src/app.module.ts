c5141a7a47654107e0508d0245dbfd8767d4121b
Boyan Zhelev <boyanz35@gmail.com>
2023-11-01 16:28:18 +0200

    chore: delete unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { FiloModule } from './filo/filo.module';
 
 @Module({
   imports: [FiloModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
