25b1ce6d6fc241a4712196ac96ea71d4df2cb38e
Boyan Zhelev <boyanz35@gmail.com>
2023-11-01 17:37:43 +0200

    feat: add controller

--- src/filo/filo.controller.ts
+++ src/filo/filo.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Post,
+  BadRequestException,
+  Query,
+} from '@nestjs/common';
+import { FiloService } from './filo.service';
 
 @Controller('filo')
-export class FiloController {}
+export class FiloController {
+  constructor(private filoService: FiloService) {}
+
+  @Post()
+  addToStack(@Query('number') numToAdd: string) {
+    const number = Number(numToAdd);
+
+    if (!number) {
+      throw new BadRequestException(
+        'Invalid Data Provided, please provide a number',
+      );
+    }
+
+    return this.filoService.addNumberToStack(number);
+  }
+
+  @Delete()
+  removeFromStack() {
+    return this.filoService.removeFromStack();
+  }
+
+  @Get()
+  getFullStack() {
+    return this.filoService.getFullStack();
+  }
+
+  @Delete('all')
+  deleteFullStack() {
+    return this.filoService.deleteFullStack();
+  }
+}
